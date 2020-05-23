0ebdec059b9c96bbd049bfd60d0511b9488e27f8
Manikanta Ambadipudi <ma185307@ncr.com>
2020-05-24 00:12:18 +0530

    Committing the library

---
+++ lib/com.py
@@ -0,0 +1,15 @@
+"""
+Folder mappings to access any specific folders from the Outlook account
+3  Deleted Items
+4  Outbox
+5  Sent Items
+6  Inbox
+9  Calendar
+10 Contacts
+11 Journal
+12 Notes
+13 Tasks
+14 Drafts
+"""
+
+import winsound

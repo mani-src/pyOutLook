f1cbbc61001b9a7dd04fb4c5691ac9c191548c30
Manikanta Ambadipudi <ma185307@ncr.com>
2020-05-24 00:05:12 +0530

    Making the first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pyOutLook',
+    version='0.1',
+    packages=['lib', 'src'],
+    install_requires=['pywin32'],
+    url='',
+    license='',
+    author='Manikanta Ambadipudi',
+    author_email='ambadipudi.manikanta@gmail.com',
+    description='Library and command line application for Outlook interface',
+)
